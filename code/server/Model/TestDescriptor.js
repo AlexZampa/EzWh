747b74e23b139bd666f9515bf46420c4ac06115e
Gallo <s259293@studenti.polito.it>
2022-05-16 14:07:58 +0200

    added testDescriptor

--- code/server/Model/TestDescriptor.js
+++ code/server/Model/TestDescriptor.js
@@ -2,12 +2,40 @@
 
 class TestDescriptor {
 
-    constructor(id){
+    constructor(id, name, procedureDescription, SKUid){
         this.id = id;
+        this.name = name;
+        this.procedureDescription = procedureDescription;
+        this.SKUid = SKUid;
     };
 
     getID = () => this.id;
+    getName = () => this.name;
+    getprocedureDescription = () => this.procedureDescription;
+    getSKUid = () => this.SKUid;
 
+    setID = (ID) => {this.id = ID; };
+    setName = (name) => {this.name = name; };
+    setProcedureDescription = (procedureDescription) => {this.procedureDescription = procedureDescription; };
+    setSKUid = (SKUid) => {this.SKUid = SKUid; };
+
+    modifyTestDescriptorData = async (newName, newProcedureDescription, newIdSKU) => {
+        try{
+            const res = await TestDescriptorDAO.updateTestDescriptor(this.id, newName, newProcedureDescription, newIdSKU);
+            this.name = newName;
+            this.procedureDescription = newProcedureDescription;
+            this.SKUid = newIdSKU;
+            return res;
+        }
+        catch(err){
+            throw err;
+        }
+    }
+
+    convertToObj = () => {
+        return (
+            {"id" : this.id, "name" : this.name, "procedureDescription" : this.procedureDescription, "SKUid" : this.SKUid});
+    };
 };
 
 module.exports = TestDescriptor;
