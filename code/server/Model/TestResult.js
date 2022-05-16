54e3844d32208c78c7f548c1f6ca4db18a0c395d
Gallo <s259293@studenti.polito.it>
2022-05-16 14:09:47 +0200

    added tesResult

--- code/server/Model/TestResult.js
+++ code/server/Model/TestResult.js
@@ -1,9 +1,36 @@
 'use strict';
 
-class TestResult{
-    constructor(){
+const dayjs = require("dayjs");
 
+class TestResult{
+
+    constructor(id, rfid, idTestDescriptor, date=undefined, result){
+        this.id = id;
+        this.rfid=rfid;
+        this.idTestDescriptor = idTestDescriptor;
+        this.date = date ? dayjs(date) : "";
+        this.result = result;
     };
+
+    modifyTestResultdata = async (newIdTestDescriptor, newDate, newResult) => {
+        try{
+            const res = await TestResultDAO.updateTestResult(this.id, this.rfid, newIdTestDescriptor, newDate, newResult);
+            this.idTestDescriptor = newIdTestDescriptor;
+            this.date = newDate ? dayjs(newDate) : "";
+            this.result = newResult;
+            return res;
+        }catch(err){
+            throw err;
+        }
+    }
+
+    convertToObj = () => {
+        return(
+            {
+                "id" : this.id, "idTestDescriptor" : this.idTestDescriptor, "date" : this.date, "result" : this.result
+            }
+        )
+    }
 };
 
 module.exports = TestResult;
